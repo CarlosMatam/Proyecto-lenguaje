fc8438c6bf26b3921cf80a89289e598138a12f8a
MarcelaMejia22 <98727538+MarcelaMejia22@users.noreply.github.com>
2022-11-21 21:16:15 -0600

    Avance del CRUD en la base de datos
    Implementación de procedimientos Almacenados.

--- Proyecto-lenguaje.sql
+++ Proyecto-lenguaje.sql
@@ -140,6 +140,20 @@ modelo varchar(15),
 marca varchar(15),
 estado varchar(15));
 
+CREATE TRIGGER trig_vehiculos_seque
+  BEFORE INSERT ON vehiculo
+  FOR EACH ROW
+  BEGIN
+    SELECT seq_vehiculo.nextval INTO :new.idVehiculo FROM dual;
+  END
+;
+
+
+CREATE SEQUENCE seq_vehiculo --nombre de la secuencia
+START WITH 1 --la secuencia empieza por 1
+INCREMENT BY 1 --se incrementa de uno en uno
+NOMAXVALUE; --no tiene valor maximo
+
 alter table vehiculo add constraint FK_id_cliente_vehiculo
 foreign key (id_cliente)
 references cliente(id_cliente);
@@ -159,6 +173,7 @@ apellido varchar(20),
 FechaIngreso date);
 
 alter table mecanicos add primary key (idMecanicos);
+insert into mecanicos values(2 ,'1234567' ,'Melo' , 'Cubero' ,'10/10/2018');
 
 -----------
 
@@ -171,8 +186,8 @@ alter table aigna_mantenimiento add constraint FK_id_mecanico
 foreign key (idMecanico)
 references mecanicos(idMecanicos);
 
-alter table aigna_mantenimiento add primary key (idMantenimiento);
-
+alter table aigna_mantenimiento add primary key (idMantenimiento , idMecanico);
+insert into aigna_mantenimiento values(1 ,2);
 -------------------------------
 drop table mantenimiento;
 create table mantenimiento(
@@ -240,24 +255,29 @@ references cliente(id_cliente);
 
 alter table Factura add primary key (idFactura);
 -----------------------------------------------------
-
+select * from mecanicos;
 -------------Principales Funcionalidades---------------------
 
-/*El administrador puede consultar la informaciÛn de los
-autos que est·n en 
-el taller, como el daÒo que presenta y cu·l mec·nico est· a cargo*/
+/*El administrador puede consultar la informaci√≥n de los
+autos que est√°n en 
+el taller, como el da√±o que presenta y cu√°l mec√°nico est√° a cargo*/
 create view  vista_VEHICULOS as
-   select e.IDVEHICULO, c.nombre ||'-'|| apellido "Cliente" ,e.PLACA  "Placa Vehiculo",m.DESCRIPCION "descripcion"
+   select e.IDVEHICULO, c.nombre ||'-'|| apellido "Cliente" ,e.PLACA  "Placa Vehiculo",m.DESCRIPCION "descripcion" ,a.idmecanico "id_Trabajador"
    from VEHICULO e  join cliente c on e.ID_CLIENTE = c.ID_CLIENTE 
-   join mantenimiento m on e.PLACA = m.PLACA;
+   join mantenimiento m on e.PLACA = m.PLACA 
+   join aigna_mantenimiento a on m.IDMANTENIMIENTO = a.IDMANTENIMIENTO
+  ;
 select * from vista_vehiculos;
 
-/*El sistema permitir· que para el cliente se le 
-ingrese sus datos personales (para efectos de facturaciÛn electrÛnica y
-tambiÈn en el caso de que deseara contactar con el mismo) asÌ tambiÈn como 
-los datos requeridos del vehÌculo; con antelaciÛn para que
-el personal del taller pueda organizar lo necesario para dar soluciÛn.*/ 
+/*El sistema permitir√° que para el cliente se le 
+ingrese sus datos personales (para efectos de facturaci√≥n electr√≥nica y
+tambi√©n en el caso de que deseara contactar con el mismo) as√≠ tambi√©n como 
+los datos requeridos del veh√≠culo; con antelaci√≥n para que
+el personal del taller pueda organizar lo necesario para dar soluci√≥n.*/ 
 ----------------------------------------------
+
+--------------------------------------------------------------Clientes-----------------------------------------
+--------------------------------------------------------Ingresar-----------------------------------
 create or replace procedure ingresar_datoss( param_cedula in VARCHAR , param_nombre in VARCHAR,
   param_apellido in VARCHAR)
 
@@ -280,6 +300,231 @@ end;
 set serveroutput on;
   ---------------------------------------------
   execute ingresar_datoss('00000','prueba','prueba');
+  -------------------------------------------
+  
+  --------------------------------------------------------Actualizar-------------------------------
+  
+  create or replace procedure actualizar_datoss_cliente(param_realizar in VARCHAR ,param_id in varchar ,param_cedula in VARCHAR , param_nombre in VARCHAR,
+  param_apellido in VARCHAR)
+
+as
+  var_cedula varchar(8);
+  var_nombre varchar(100);
+  var_apellido varchar(100);
+  var_resultado varchar(100);
+  
+  
+begin
+
+var_cedula :=param_cedula;
+var_nombre :=param_nombre;
+var_apellido := param_apellido;
+var_resultado :='Los datos fueron actualizados de manera correcta';
+
+  if (param_realizar = 'nombre') then
+    update cliente set nombre =var_nombre
+    where id_cliente= param_id;
+     dbms_output.put_line(var_resultado);
+    
+    elsif (param_realizar ='cedula') then
+      update cliente set cedula = var_cedula
+      where id_cliente= param_id;
+      dbms_output.put_line(var_resultado);
+      
+    elsif(param_realizar ='apellido') then
+      update cliente set apellido = var_apellido
+      where id_cliente= param_id;
+      dbms_output.put_line(var_resultado);
+      
+      elsif(param_realizar ='.') then
+      update cliente set cedula = var_cedula , nombre =var_nombre ,apellido = var_apellido
+      where id_cliente= param_id;
+      dbms_output.put_line(var_resultado);
+    else 
+        dbms_output.put_line('Accion incorrecta');       
+        end if;    
+end;
+set serveroutput on;
+  ---------------------------------------------
+  /*para cambiar nada mas la cedula*/
+  execute actualizar_datoss_cliente('cedula',1,'12222','' ,'');
+  
+  /*para cambiar nombre*/
+  execute actualizar_datoss_cliente('nombre', 1,'','pruebaAC2super' ,'');
+  
+   /*para cambiar apellido*/
+  execute actualizar_datoss_cliente('apellido', 1,'','' ,'Mejorado');
+  
+   /*para cambiar apellido*/
+  execute actualizar_datoss_cliente('.', 1,'3333','todo' ,'todos');
+  ------------------------------------------------------------
+  
+  create or replace procedure delete_datoss_clientes(param_id in varchar)
+
+as
+  
+  var_resultado varchar(100);
+  
+begin
+
+var_resultado :='Los datos fueron eliminados de manera correcta';
+  delete from cliente where id_cliente = param_id;
+   dbms_output.put_line(var_resultado);
+end;
+set serveroutput on;
+  ---------------------------------------------
+  execute delete_datoss_clientes(1);
+  -------------------------------------------
+select * from cliente;
+
+-------------------------------------------Vehiculos------------------------------------------------
+
+create or replace procedure ingresar_datoss_vehiculo( param_id_cliente in number , param_placa in VARCHAR,
+  param_modelo in VARCHAR, param_marca in VARCHAR , param_tipovehiculo in VARCHAR)
+
+as
+  var_id_cliente INTEGER;
+  var_placa varchar(6);
+  var_modelo varchar(15);
+  var_marca varchar(15);
+  var_tipovehiculo varchar(15);
+  var_resultado varchar(100);
+  
+begin
+
+var_id_cliente :=param_id_cliente;
+var_placa := param_placa;
+var_modelo :=param_modelo;
+var_marca :=param_marca;
+var_tipovehiculo :=param_tipovehiculo;
+var_resultado :='Los datos fueron ingresados de manera correcta';
+
+  insert into vehiculo(id_cliente ,placa, modelo , marca , tipovehiculo)values(var_id_cliente,var_placa , var_modelo , var_marca , var_tipovehiculo);
+  dbms_output.put_line(var_resultado);
+end;
+set serveroutput on;
+  ---------------------------------------------
+  execute ingresar_datoss_vehiculo(2,'11','2x2' , 'amarok' ,'7 puertas');
+  -------------------------------------------
+  select * from vehiculo;
+  select * from cliente;
+  ------------------------------Actualizar------------------------------------------------------
+  /* faltaaaaaaaaaaaaaaaa
+  create or replace procedure actualizar_datoss_vehiculo(param_realizar in VARCHAR ,param_id in varchar ,param_id_cliente in VARCHAR , param_placa in VARCHAR,
+  param_modelo in VARCHAR , param_marca in VARCHAR , param_tipovehiculo in VARCHAR)
+
+as
+  var_id_cliente int;
+  var_resultado varchar(100); 
+begin
+
+var_id_cliente := param_id_cliente;
+var_resultado :='Los datos fueron actualizados de manera correcta';
+
+  if (param_realizar = 'cliente') then
+    update vehiculo set id_cliente =var_id_cliente
+    where idvehiculo= param_id;
+     dbms_output.put_line(var_resultado);
+    
+    elsif (param_realizar ='placa') then
+      update vehiculo set placa = param_placa
+      where idvehiculo= param_id;
+      dbms_output.put_line(var_resultado);
+      
+    elsif(param_realizar ='modelo') then
+      update vehiculo set modelo = param_modelo
+      where idvehiculo= param_id;
+      dbms_output.put_line(var_resultado);
+      
+       elsif(param_realizar ='marca') then
+      update vehiculo set marca = param_marca
+      where idvehiculo= param_id;
+      dbms_output.put_line(var_resultado);
+      
+       elsif(param_realizar ='tipo') then
+      update vehiculo set tipovehiculo = param_tipovehiculo
+      where idvehiculo= param_id;
+      dbms_output.put_line(var_resultado);
+      
+      elsif(param_realizar ='.') then
+      update vehiculo set id_cliente = param_id_cliente , placa = param_placa ,modelo = param_modelo , marca=param_marca , tipovehiculo =param_tipovehiculo
+      where idvehiculo= param_id;
+      dbms_output.put_line(var_resultado);
+    else 
+        dbms_output.put_line('Accion incorrecta');       
+        end if;    
+end;
+set serveroutput on;*/
+  ---------------------------------------------
+  /*para cambiar nada mas el id cliente*/
+  execute actualizar_datoss_vehiculo('cliente',2,46,'' ,'' , '','');
+  select * from vehiculo;
+  
+  select * from cliente;
+  
+  /*para cambiar nombre*/
+  execute actualizar_datoss_vehiculo('placa', 6,1,'222' ,'','','');
+  
+   /*para cambiar apellido*/
+  execute actualizar_datoss_cliente('apellido', 1,'','' ,'Mejorado');
+  
+   /*para cambiar apellido*/
+  execute actualizar_datoss_cliente('.', 1,'3333','todo' ,'todos');
+  ------------------------------------------------------------
+  
+  create or replace procedure ingresar_datos_mantenimiento( param_placa in VARCHAR , param_descripcion in VARCHAR,
+  param_fecha_ingreso in date)
+
+as
+  var_placa varchar(8);
+  var_descripcion varchar(100);
+  var_ingreso date;
+  var_id int;
+  var_resultado varchar(80);
+begin
+select max(idMantenimiento)+ 1 into var_id from mantenimiento;
+var_placa :=param_placa;
+var_descripcion :=param_descripcion;
+var_ingreso := param_fecha_ingreso;
+
+var_resultado :='Se inserto el mantenimiento exitosamente';
+  insert into cliente(idMantenimiento , placa, descripcion,fechaHoraLL,fechaHoraSA)values(var_id,var_placa,var_descripcion,var_ingreso,var_ingreso);
+  dbms_output.put_line(var_resultado);
+end;
+  
+  set serveroutput on;
+  ---------------------------------------------
+  execute ingresar_datos_mantenimiento('hjt-lkj','Cambio de aceite','2022-11-21');
+
+
+-------------------------------------------------------------------------------------------
+  create or replace procedure ingresar_datos_telefonos( cliente in int, tipo varchar2,
+  numero in varchar2)
+as
+
+  var_resultado varchar2(100);
+begin
+
+  var_resultado :='Se inserto el numero de telefono correctamente';
+  insert into telefonos(id_cliente , tipoTelefono, numero) values (cliente,tipo , numero);
+  dbms_output.put_line(var_resultado);
+end;
+
+------------------------------------------------------------------------------------------------
+
+create or replace procedure actualizar_datos_mantenimiento( idMant in int ,param_placa in VARCHAR , param_descripcion in VARCHAR,
+  param_fecha_ingreso in date,param_fecha_salida in date)
+
+as
+  var_resultado varchar(80);
+begin
+
+
+var_resultado :='El mantenimiento se actualizo correctamente';
+ update mantenimiento set placa = param_placa, descripcion = param_descripcion,fechaHoraLL = param_fecha_ingreso ,fechaHoraSA =
+ param_fecha_salida where idMantenimiento =idMant;
+  dbms_output.put_line(var_resultado);
+end;
   -------------------------------------------
   
 
